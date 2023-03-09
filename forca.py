822c27ddfebbd5226c53e433c35bbfea1b02ba9e
PolegolasXD <polegolasplays@gmail.com>
2023-03-09 19:32:16 -0300

    feat: adicionando novo jogo -adicionando jogo da forca

---
+++ forca.py
@@ -0,0 +1,8 @@
+
+def jogar():
+
+    print("-------------------------------------")
+    print("seja bem vindo ao jogo da forca")
+    print("-------------------------------------")
+
+    print("fim de jogo")
