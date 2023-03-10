d765a107b1d446bf9fd5ad796b4544d60f6811ea
PolegolasXD <polegolasplays@gmail.com>
2023-03-10 19:53:47 -0300

    feat: criando logica do jogo forca -criando e optimizando a logica do jogo forca

--- forca.py
+++ forca.py
@@ -3,7 +3,23 @@ def jogar():
 
     print("-------------------------------------")
     print("seja bem vindo ao jogo da forca")
-    print("-------------------------------------")
+    print("-------------------------------------\n")
+
+    palavraSecreta = "batata"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra escolhida: ")
+        index = 0
+        for letra in palavraSecreta:
+            if(chute == letra):
+                print(f"Encontrei a letra '{chute}' na posição '{index}'")
+            index = index + 1
+
+        print("jogando ...")
 
     print("fim de jogo")
     
